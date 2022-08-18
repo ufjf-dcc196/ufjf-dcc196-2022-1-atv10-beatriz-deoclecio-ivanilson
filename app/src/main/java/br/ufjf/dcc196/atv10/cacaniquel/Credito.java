0c47973bb65627a0c7532b409d496bfb087e6317
beatrizcunharo <beatrizcunharo@gmail.com>
2022-08-17 23:15:36 -0300

    feat: Ajustes na interface e adição de funcionalidades para roleta do caça níquel

--- app/src/main/java/br/ufjf/dcc196/atv10/cacaniquel/Credito.java
+++ app/src/main/java/br/ufjf/dcc196/atv10/cacaniquel/Credito.java
@@ -1,14 +1,24 @@
-package br.ufjf.dcc196.atv10.cacaniquel;
+package br.ufjf.dcc196.deocleciofilho.cacaniquel3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Credito extends AppCompatActivity {
 
+    Button btnVoltar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_credito);
+        btnVoltar=findViewById(R.id.btnVoltar);
+        btnVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
