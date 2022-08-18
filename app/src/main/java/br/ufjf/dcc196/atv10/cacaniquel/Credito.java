913bfa3bbf08f7ad16083972d2a883d427d5c0f5
Ivanylson <ivanylson.honorio.goncalves@ice.ufjf.br>
2022-08-18 01:00:09 -0300

    Refatorando a classe Credito

--- app/src/main/java/br/ufjf/dcc196/atv10/cacaniquel/Credito.java
+++ app/src/main/java/br/ufjf/dcc196/atv10/cacaniquel/Credito.java
@@ -4,10 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+/**Atividade 10 - Caça Níquel
+ Discentes:
 
+ Beatriz Cunha Rodrigues - 201776038
+
+ Deoclécio Porfirio Ferreira Filho - 201876043
+
+ Ivanylson Honório Gonçalves - 201776002
+ * **/
+/**Classe Credito serve para mostrar o nome dos integrantes**/
 public class Credito extends AppCompatActivity {
-
+    //Criando o botato
     Button btnVoltar;
+    //Chamando o criação do activity credito
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
