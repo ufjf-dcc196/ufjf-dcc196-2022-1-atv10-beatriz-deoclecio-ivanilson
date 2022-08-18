cbe886a68894094a09fc0fe286f827e6a4f162a5
Deoclécio Filho <deoclecio.filho@estudante.ufjf.br>
2022-08-17 21:59:01 -0300

    Criado a activity_jogar e a activity_credito.xml

--- app/src/main/java/br/ufjf/dcc196/atv10/cacaniquel/MainActivity.java
+++ app/src/main/java/br/ufjf/dcc196/atv10/cacaniquel/MainActivity.java
@@ -2,13 +2,50 @@ package br.ufjf.dcc196.atv10.cacaniquel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView txtNome;
+    Button btnEntrar;
+    Button btnFechar;
+    Button btnEquipe;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtNome = findViewById(R.id.txtNome);
+        btnEntrar = findViewById(R.id.btnVoltar);
+        btnFechar = findViewById(R.id.btnFechar);
+        btnEquipe = findViewById(R.id.btnEquipe);
+
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                 Intent intent = new Intent(MainActivity.this, Jogar.class);
+                startActivity(intent);
+            }
+        });
+
+        btnEquipe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                    Intent intent = new Intent(MainActivity.this, Credito.class);
+                    startActivity(intent);
+            }
+        });
+
+        btnFechar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
